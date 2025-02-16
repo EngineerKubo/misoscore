26547646eb2d601611bc2813a8dacfb781fef2ba
EngineerKubo <166462607+EngineerKubo@users.noreply.github.com>
2025-02-17 04:19:46 +0900

    コントローラー設定 カロリー計算

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,5 +4,8 @@ class IngredientsController < ApplicationController
   end
 
   def calculate
+    selected_names = params[:ingredients] || []
+    @selected_ingredients = Ingredient.all.select { |ing| selected_names.include?(ing.name) }
+    @total_calories = @selected_ingredients.sum(&:calories)
   end
 end
