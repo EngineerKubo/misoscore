9452a498afd23f25b06cd0cea362e8aa7f1c119d
EngineerKubo <166462607+EngineerKubo@users.noreply.github.com>
2025-02-17 07:33:39 +0900

    修正 リロード時のエラー

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,11 +1,16 @@
 class IngredientsController < ApplicationController
   def index
     @ingredients = Ingredient.all
-    Rails.logger.debug "Ingredients Data: #{@ingredients.inspect}"
   end
 
   def calculate
     selected_names = params[:ingredients] || []
+    session[:selected_ingredients] = selected_names  # 選択データをセッションに保存
+    redirect_to ingredients_result_path  # `result` ページにリダイレクト
+  end
+
+  def result
+    selected_names = session[:selected_ingredients] || []
     @selected_ingredients = Ingredient.all.select { |ing| selected_names.include?(ing.name) }
     @total_calories = @selected_ingredients.sum(&:calories)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'ingredients#index'
   post 'ingredients/calculate', to: 'ingredients#calculate'
+  get 'ingredients/result', to: 'ingredients#result'  # ← 修正
 end
