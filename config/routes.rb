6f03384fec1adebaefae75f4fd8b70ad1f894c88
EngineerKubo <166462607+EngineerKubo@users.noreply.github.com>
2025-02-17 04:31:13 +0900

    ルーティング修正 ルート (root) の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'ingredients/index'
+  root 'ingredients#index'
   post 'calculate', to: 'ingredients#calculate'
 
   get "up" => "rails/health#show", as: :rails_health_check
